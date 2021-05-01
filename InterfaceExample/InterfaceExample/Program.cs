a7f023e3b59c6511fba791c58e18ea85e54bf6ef
Artyom-Gerchik-053506 <>
2021-05-01 10:03:56 +0300

    InterfaceExample initial commit

---
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    interface IFirst
+    {
+        int SomeMethod();
+    }
+
+    interface ISecondToWorkWithIFirst
+    {
+        void DoSomething(IFirst iFirstThing);
+    }
+
+    class WorkWithInterfaces : ISecondToWorkWithIFirst
+    {
+        public void DoSomething(IFirst iFirstThing)
+        {
+            Console.WriteLine(iFirstThing.SomeMethod());
+        }
+    }
+
+    class WorkWithIFirstFirst : IFirst
+    {
+        public int SomeMethod()
+        {
+            return 1;
+        }
+    }
+
+    class WorkWithIFirstTen : IFirst
+    {
+        public int SomeMethod()
+        {
+            return 10;
+        }
+    }
+
+    class WorkWithIFirstFifty : IFirst
+    {
+        public int SomeMethod()
+        {
+            return 50;
+        }
+    }
+
+    class WorkWithFirstRandom : IFirst
+    {
+        public int SomeMethod()
+        {
+            Random randomInt = new Random();
+            return randomInt.Next(0, 100500);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ISecondToWorkWithIFirst testMe = new WorkWithInterfaces();
+            testMe.DoSomething(new WorkWithIFirstFirst());
+            testMe.DoSomething(new WorkWithIFirstTen());
+            testMe.DoSomething(new WorkWithIFirstFifty());
+            testMe.DoSomething(new WorkWithFirstRandom());
+        }
+    }
+}
